0d52590ef460e91facd6afcae72701fe1f235e91
Victoria Isachenko <funn.willow@gmail.com>
2023-08-31 18:23:21 +0600

    Initial commit.

---
+++ src/BmiService.java
@@ -0,0 +1,6 @@
+public class BmiService {
+    public int calculate(double weightInKg, double heightInMeters) {
+        double bmi = weightInKg / (heightInMeters * heightInMeters);
+        return (int) bmi;
+    }
+}
